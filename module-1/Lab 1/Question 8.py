6c8c47f7ff75915d8382978102184fc896aa02a8
mikelhomeless <mthomer1027@gmail.com>
2019-10-14 22:45:13 -0500

    yeet

--- module-1/Lab 1/Question 8.py
+++ module-1/Lab 1/Question 8.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 train = pd.read_csv('winequality-red.csv')
@@ -26,10 +27,28 @@ print ('RMSE is: \n', mean_squared_error(y_test, predictions))
 
 numeric_features = train.select_dtypes(include=[np.number])
 corr = numeric_features.corr()
-print (corr['quality'].sort_values(ascending=False)[:4], '\n')
+print (corr['quality'].sort_values(ascending=False)[:], '\n')
 
 ##Null values
 nulls = pd.DataFrame(train.isnull().sum().sort_values(ascending=False)[:25])
 nulls.columns = ['Null Count']
 nulls.index.name = 'Feature'
 print(nulls)
+
+# pivot and view all features
+for feature in train.columns[:-1]:
+    pivot = train.pivot_table(index=feature, values='quality', aggfunc=np.median)
+    pivot.plot(kind='bar', color='blue')
+    plt.show()
+
+# collect only the features which are positively corrolated
+revised_dataset = train[['alcohol', 'sulphates', 'citric acid', 'fixed acidity', 'residual sugar', 'quality']]
+y = np.log(revised_dataset.quality)
+x = revised_dataset.drop(['quality'], axis=1)
+x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=42, test_size=.33)
+lr = linear_model.LinearRegression()
+lr.fit(x_train, y_train)
+predictions = lr.predict(x_test)
+print ("R^2 is: \n", lr.score(x_test, y_test))
+print ('RMSE is: \n', mean_squared_error(y_test, predictions))
+# It appears that the model did not improve, even with EDA
